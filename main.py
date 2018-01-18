e3622cdbb4018800ef9ed30af7fc45087663e13a
MadDoxXimba <jonathan.yue-chun@etu.univ-nantes.fr>
2018-01-18 19:01:22 +0000

    example code of flast get, post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +8,11 @@ def welcomePage():
 @app.route("/test")
 def testPage():
     return "<strong style=\"'color':red\"> Ceci est un test...</strong>"
+    
+@app.route("/query/<data>", methods=['GET', 'POST'])
+def queryParser(data):
+    if request.method == 'GET':
+        return request.path
+    else:
+        return str(data)
+        
