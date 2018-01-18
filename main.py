1f49bfb3cc27678ca90cf97034c3b80c635fd587
MadDoxXimba <jonathan.yue-chun@etu.univ-nantes.fr>
2018-01-18 21:28:24 +0000

    added views html and form example query

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from databaseManager import connection
+import os
+import sys
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader('main', 'views'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def welcomePage():
-    return "<strong>TP WEB SEMANTIQUE: Jonathan YUE CHUN, Valentin Bouchevreau, Quentin Levavasseur</strong>"
+    template = env.get_template('queryform.html')
+    return render_template(template)
     
 @app.route("/test")
 def testPage():
@@ -18,4 +27,17 @@ def queryParser(data):
         return request.path
     else:
         return str(data)
+        
+@app.route("/result", methods=['GET', 'POST'])
+def getForm():
+    if request.method == 'POST':
+      result = request.form
+      # logic (db connection and sparkl query parser)
+      
+      
+      
+      # result for user
+      template = env.get_template('result.html')
+      return render_template(template, result = result)
+    
         
