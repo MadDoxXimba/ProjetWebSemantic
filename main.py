29ded0012e1b0e4b7a5ea5711a252b33a80c4d2c
levavasq <quentin.levavasseur@etu.univ-nantes.fr>
2018-02-07 18:25:33 +0100

    test affichage JSON 12

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def getForm():
         sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/query")
         #sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/sparql")
         #sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/update")
-        
+        '''
         # QUERY THE SERVER
         query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
         query += "PREFIX n1: <https://tpws/> "
@@ -57,18 +57,31 @@ def getForm():
         query += "?ville_84 n1:nom ?nom_103 . "
         query += "?nom_103 rdfs:label '"+str(result)+"' . } "
         query += "LIMIT 200"
+        '''
 
-		#sparql.setQuery(query)
+		sparql.setQuery("""
+		    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+		    PREFIX n1: <https://tpws/>
+            SELECT DISTINCT ?offre_22 ?possede_43 ?situeA_65 ?ville_84 ?nom_103
+            WHERE { ?offre_22 a n1:offre .
+            ?possede_43 a n1:contact .
+            ?offre_22 n1:possede ?possede_43 .
+            ?possede_43 n1:situeA ?situeA_65 .
+            ?situeA_65 n1:ville ?ville_84 .
+            ?ville_84 n1:nom ?nom_103 .
+            ?nom_103 rdfs:label '"""+str(result)+"""' . }
+			LIMIT 200
+		""")
 		
         # RESPONSE FROM SERVER
         
-        #sparql.setReturnFormat(JSON)
-        #results = sparql.query().convert()
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
         
         # JSON FORMAT
         
-        #print(results)
-        print(query)
+        print(results)
+        #print(query)
 
         # result for user
         template = env.get_template('result.html')
