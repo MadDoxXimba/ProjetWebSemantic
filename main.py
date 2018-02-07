bb3f1dbbfbc783d6f1c3a89128a5085e388d787d
MadDoxXimba <jonathan.yue-chun@etu.univ-nantes.fr>
2018-02-07 19:34:13 +0000

    fix vis bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template
 from databaseManager import connection
 from SPARQLWrapper import SPARQLWrapper, JSON
 import os
