6384eb4a5a80c43869493e22349f37af107ca1e0
levavasq <quentin.levavasseur@etu.univ-nantes.fr>
2018-02-08 20:28:31 +0100

    listOffers graph test

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ env = Environment(
 
 app = Flask(__name__)
 
+class mydict(dict):
+    def __str__(self):
+        return json.dumps(self)
 
 @app.route("/")
 def welcomePage():
@@ -461,66 +464,10 @@ def getForm():
             edges.append({"from": cpt, "to": 0})
             cpt = cpt +1
 
-        nodes = [n["label"].replace("'",'"') for n in nodes]
 
+        nodes = mydict(nodes)
 
         # result for user
         template = env.get_template('result.html')
         
     return render_template(template, city = str(result[0]), offers = listOffers, result1 = edges, result2 = nodes)
-
-@app.route("/graphOffersByCity", methods=['GET', 'POST'])
-def graphOffersByCity():
-    if request.method == 'POST':
-
-        result = request.form.getlist('key')
-
-        sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/query")
-        #sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/sparql")
-        #sparql = SPARQLWrapper("https://herokufuseki.herokuapp.com/WebSemantic/update")
-
-        # QUERY THE SERVER
-
-        sparql.setQuery("""PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX n1: <https://tpws/>
-            SELECT DISTINCT ?label_100 
-            WHERE { ?offre_22 a n1:offre .
-            ?offre_22 n1:nom ?nom_100 .
-            ?nom_100 rdfs:label ?label_100 .
-            ?possede_43 a n1:contact .
-            ?offre_22 n1:possede ?possede_43 .
-            ?possede_43 n1:situeA ?situeA_65 .
-            ?situeA_65 n1:ville ?ville_84 .
-            ?ville_84 n1:nom ?nom_103 .
-            ?nom_103 rdfs:label '"""+str(result[0])+"""' . }
-            ORDER BY ?label_100
-            LIMIT 100
-        """)
-
-        # RESPONSE FROM SERVER
-        # JSON FORMAT
-
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-
-        # JSON FORMAT
-
-        listOffers=[]
-        for obj in results['results']['bindings']:
-            listOffers.append(obj['label_100']['value'])
-
-        template = env.get_template('graph.html')
-
-        edges = []
-        nodes = [{"id": 0, "label": result[0], "group": 1}]
-
-        cpt = 1
-        for o in listOffers:
-            nodes.append({"id": cpt, "label": o, "group": 2})
-            edges.append({"from": cpt, "to": 0})
-            cpt = cpt +1
-
-        nodes = [n.replace("'",'"') for n in nodes]
-        edges = [e.replace("'",'"') for e in edges]
-
-        return render_template(template, result1 = edges, result2 = nodes)
