a27f962fdef5150ecc148c02e637d12d31564b7a
levavasq <quentin.levavasseur@etu.univ-nantes.fr>
2018-02-10 13:26:29 +0100

    Maj flèches graphes

--- main.py
+++ main.py
@@ -204,15 +204,15 @@ def getForm():
         edges = []
         nodes = [{"id": 0, "label": result[0], "group": 1}]
 
-        print(listOffers)
-        print(listOffers[1].split('/'))
-        print(listOffers[1].split('/')[0])
-        print(listOffers[1].split('/')[1])
+        print(listOffers[0][0])
+        print(listOffers[0][1].split('/'))
+        print(listOffers[0][1].split('/')[0])
+        print(listOffers[0][1].split('/')[1])
 
         cpt = 1
         for o in listOffers:
-            nodes.append({"id": cpt, "label": listOffers[0], "group": 2})
-            edges.append({"arrows": "from", "from": 0, "to": cpt, "label": listOffers[1].split('/')[2], "font": {"align": 'bottom'}})
+            nodes.append({"id": cpt, "label": listOffers[cpt-1][0], "group": 2})
+            edges.append({"arrows": "from", "from": 0, "to": cpt, "label": listOffers[cpt-1][1].split('/')[3], "font": {"align": 'bottom'}})
             cpt = cpt +1
 
 
